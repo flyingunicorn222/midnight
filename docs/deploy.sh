de15af67b1b813b5cc7da2aedfcf14588dac3e3f
flyingunicorn222 <flyingunicorn222@user.noreply.github.com>
2017-03-05 15:18:17 -0300

    updating deploy routine

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -4,6 +4,8 @@ gitbook build
 cd _book
 rm -rf deploy.sh
 git init
+git config user.name "flyinunicorn222"
+git config user.email "flyingunicorn222@user.noreply.github.com"
 git add -A
 git commit -m 'latest docs'
 git push -f https://github.com/flyingunicorn222/telebroker_bot.git master:gh-pages
